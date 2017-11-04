44a81fb8b6b9ae8b9f14ba15a3fb4cdc3895378e
BC <chen.b.brian@gmail.com>
2017-11-03 23:26:55 -0400

    Tests

--- src/test/java/TicketServiceTest.java
+++ src/test/java/TicketServiceTest.java
@@ -1,36 +1,43 @@
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.UUID;
 
 public class TicketServiceTest {
+    private Venue venue = new Venue(10,10);
+
     public static void main(String[] args) {
-        Venue venue = new Venue(10,10);
+        TicketServiceTest tst = new TicketServiceTest();
+
+        //SeatHold sarah = venue.findAndHoldSeats(4, "sarah@gmail.com");
+        tst.assertReserveHold();
+        tst.assertBestSeatsObject();
+        tst.assertConfirmReserve();
 
 
-        SeatHold sarah = venue.findAndHoldSeats(4, "sarah@gmail.com");
-        TicketServiceTest.assertReserveHold(venue, "sarah@gmail.com");
-        TicketServiceTest.assertBestSeatsObject(venue, sarah.getSeatHoldID(), "sarah@gmail.com");
-        TicketServiceTest.assertConfirmReserve(venue, sarah.getSeatHoldID(), "sarah@gmail.com");
-
-
-        venue = new Venue(10, 10);
-        TicketServiceTest.assertHoldRelease(venue, 5, "sarah@gmail.com");
-
-
-
+        //venue = new Venue(10, 10);
+        tst.assertHoldRelease();
 
 
     }
 
-    private static void assertConfirmReserve(Venue venue, UUID SeatHoldID, String customerEmail) {
-        String conf = venue.reserveSeats(SeatHoldID, customerEmail);
+    @Test
+    public void assertConfirmReserve() {
+        SeatHold sarah = venue.findAndHoldSeats(4, "sarah@gmail.com");
+        UUID SeatHoldID = sarah.getSeatHoldID();
+        String conf = venue.reserveSeats(SeatHoldID, "sarah@gmail.com");
         String testconf = "sarah@gmail.comconfirmed";
         if( !conf.equals(testconf)) {
             throw new AssertionError("Reserved confirmation error");
         }
     }
 
+    @Test
+    public void assertBestSeatsObject() {
 
-    private static void assertBestSeatsObject(Venue venue, UUID SeatHoldID, String customerEmail) {
+        SeatHold sarah = venue.findAndHoldSeats(4, "sarah@gmail.com");
+        UUID SeatHoldID = sarah.getSeatHoldID();
+        String customerEmail = "sarah@gmail.com";
         ArrayList<Seat> testseats = new ArrayList<>();
         testseats.add(new Seat(0, 5));
         testseats.add(new Seat(1, 5));
@@ -47,8 +54,9 @@ public class TicketServiceTest {
         }
     }
 
-    private static void assertReserveHold(Venue venue, String customerEmail) {
-
+    @Test
+    public void assertReserveHold() {
+        SeatHold sarah = venue.findAndHoldSeats(4, "sarah@gmail.com");
         System.out.println(venue.numSeatsAvailable());
         if (venue.numSeatsAvailable() != 96) {
             throw new AssertionError("Seats not removing");
@@ -56,9 +64,9 @@ public class TicketServiceTest {
     }
 
 
-
-    private static void assertHoldRelease(Venue venue, int number, String customerEmail) {
-        SeatHold sarah = venue.findAndHoldSeats(number, "sarah@gmail.com");
+    @Test
+    public void assertHoldRelease() {
+        SeatHold sarah = venue.findAndHoldSeats(5, "sarah@gmail.com");
 
         new java.util.Timer().schedule(
                 new java.util.TimerTask() {
